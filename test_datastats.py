16697cdc6580653aac7e3d6b494279b16a3b08d6
laya <layadelcarmen@gmail.com>
2021-10-14 13:48:24 +0200

    separate the logic in class DataStats
    - create _stat function
    - create a new test test__stats

--- test_datastats.py
+++ test_datastats.py
@@ -6,55 +6,61 @@ from datastats.datastats import DataStats
 
 class TestStats(unittest.TestCase):
 
-    def test_json(self):
-        test_data = [
-            {
-                "id": 1,
-                "name": "Laura",
-                "surname": "Wagner",
-                "age": 58,
-                "salary": "€36000"
-            },
-            {
-                "id": 2,
-                "name": "Mike",
-                "surname": "Schneider",
-                "age": 45,
-                "salary": "€57300"
-            },
-            {
+    test_data = [
+        {
+            "id": 1,
+            "name": "Laura",
+            "surname": "Wagner",
+            "age": 58,
+            "salary": "€36000"
+        },
+        {
+            "id": 2,
+            "name": "Mike",
+            "surname": "Schneider",
+            "age": 45,
+            "salary": "€57300"
+        },
+        {
+            "id": 3,
+            "name": "Bern",
+            "surname": "Hellebrand",
+            "age": 65,
+            "salary": "€80500"
+        }
+    ]
+
+    vr_data = {
+            'avg_age': 56,
+            'avg_salary': 57933,
+            'avg_yearly_increase': 1053,
+            'max_salary': [{
                 "id": 3,
                 "name": "Bern",
                 "surname": "Hellebrand",
                 "age": 65,
                 "salary": "€80500"
-            }
-        ]
+            }],
+            'min_salary': [{
+                "id": 1,
+                "name": "Laura",
+                "surname": "Wagner",
+                "age": 58,
+                "salary": "€36000"
+            }]
+        }
 
-        vr_data = {
-                'avg_age': 56,
-                'avg_salary': 57933,
-                'avg_yearly_increase': 1053,
-                'max_salary': [{
-                    "id": 3,
-                    "name": "Bern",
-                    "surname": "Hellebrand",
-                    "age": 65,
-                    "salary": "€80500"
-                }],
-                'min_salary': [{
-                    "id": 1,
-                    "name": "Laura",
-                    "surname": "Wagner",
-                    "age": 58,
-                    "salary": "€36000"
-                }]
-            }
+    def test_json(self):
 
         ds = DataStats()
+        ds_result = json.loads(ds.stats(self.test_data, 20, 20000))
+        self.assertDictEqual(ds_result, self.vr_data)
 
-        ds_result = json.loads(ds.stats(test_data, 20, 20000))
-        self.assertDictEqual(ds_result, vr_data)
+
+    def test__stats(self):
+        
+        ds = DataStats()
+        self.assertDictEqual(ds._stats(self.test_data, 20, 20000), self.vr_data)
 
 
         
